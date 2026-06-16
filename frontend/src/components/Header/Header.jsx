2b4a4c9863202284375295a47d001183489098b4
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-06-16 19:42:55 +0530

    created Header component and setup project and installed material ui

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+            <img
+                className="header__logo"
+                src="logo512.png"
+                alt="IMAGE NOR WORKING"
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header;
